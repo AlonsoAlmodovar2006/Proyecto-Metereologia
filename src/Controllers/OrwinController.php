9dec3661bed04bbc8a687dbd9c0d18cbf2a034cf
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-12-16 08:56:53 +0100

    Controlador y Base de datos para las ultimas 24

--- src/Controllers/OrwinController.php
+++ src/Controllers/OrwinController.php
@@ -4,18 +4,28 @@ namespace App\Controllers;
 
 use App\Models\Database;
 use Dotenv\Dotenv;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-class Controller
+class OrwinController
 {
     private $myModel;
+    private $twig;
 
     public function __construct()
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . "/../..");
         $dotenv->load();
 
+        $loader = new FilesystemLoader(__DIR__ . "/../Views");
+        $this->twig = new Environment($loader);
+
         $this->myModel = new Database($_ENV["DB_HOST"], $_ENV["DB_PORT"], $_ENV["DB_DATABASE"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
     }
-
-    public function index() {}
+    
+    public function obtenerDatosUltimas24h()
+    {
+        echo $this->twig->render("home.html.twig");
+        // print_r($this->myModel->pedirUltimas24h());
+    }
 }
--- src/Models/Database.php
+++ src/Models/Database.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use App\Models\Datos1;
 use App\Models\Precipitacion;
+use DateTime;
+use Illuminate\Support\Facades\Date;
 
 class Database
 {
@@ -32,7 +34,12 @@ class Database
         }
     }
 
-    public function listarPrecipitacion() {
-        return $precipitaciones = Precipitacion::all();
+    public function pedirUltimas24h()
+    {
+        $fechaActual = new DateTime();
+        $fecha24hAtras = new DateTime();
+        $fecha24hAtras->modify("-24 hours");
+
+        return Datos::whereBetween("fechaSistema", [$fecha24hAtras, $fechaActual])->get();
     }
 }
